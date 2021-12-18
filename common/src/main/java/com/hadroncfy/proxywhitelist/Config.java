358e871a071072ce5975583505bc629409a59c66
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2021-12-18 17:50:04 +0800

    Change KickMsg

--- common/src/main/java/com/hadroncfy/proxywhitelist/Config.java
+++ common/src/main/java/com/hadroncfy/proxywhitelist/Config.java
@@ -1,7 +1,7 @@
 package com.hadroncfy.proxywhitelist;
 
 public class Config {
-    public String kickMessage = "Vous n'étes pas dans la liste blanche!";
+    public String kickMessage = "你不在白名单内!";
     public boolean enabled = false;
     public void assign(Config c){
         kickMessage = c.kickMessage;
